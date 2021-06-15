d07273f547c907b8340293fe8933c1e33399e2d5
Shubham Thapa <66845967+mu745511@users.noreply.github.com>
2021-06-16 00:57:48 +0530

    staticfiles_whitenoise

--- covid/settings.py
+++ covid/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = (
 
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
